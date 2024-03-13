18593f48a0148ccd7a655b08dd7dc28760a56353
David-patrick-chuks <pd3072894@gmail.com>
2024-03-13 12:27:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#FF6464',
+        'secondary':'#21243D',
+        'light':'#8695A4'
+        
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
